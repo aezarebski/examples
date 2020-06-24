51dd848bfafb84183b2466ce1fca84cbbfa58c99
Alex Zarebski <aezarebski@gmail.com>
2020-06-24 10:54:09 +0100

    demo how to switch out the data sets

--- data-loading/data-access-csv.R
+++ data-loading/data-access-csv.R
@@ -1,5 +1,6 @@
 ## This script emulates the behaviour of the ipython notebook of the same name.
 
+library(httr)
 library(dplyr)
 library(ggplot2) # <--- ggplot is one of the killer apps of R.
 
@@ -13,9 +14,37 @@ url_str <- sprintf("https://raw.githubusercontent.com/covid19db/data/master/data
 ## the main packages that are used now are oriented towards its use.
 df_epidemiology <- read.csv(url_str,
                             stringsAsFactors = FALSE) %>%
-    select(country, date, confirmed, dead) %>%
-    mutate(date = as.Date(date, format = "%d-%m-%y")) %>%
-    filter(country == "United_Kingdom")
+    select(date, confirmed, dead, country) %>%
+    mutate(date = as.Date(date, format = "%d-%m-%Y")) %>%
+    rename(cases = confirmed,
+           cumulative_deaths = dead)
+
+
+## To construct the daily deaths we need the difference of the cumulative
+## values, but that means we need to compute the difference in this which is
+## country specific so we need to do a bit of munging.
+
+daily_deaths_vector_factory <- function(df) {
+    function(country_name) {
+        df %>%
+            filter(country == country_name) %>%
+            use_series("cumulative_death") %>%
+            rev %>%
+            diff %>%
+            {c(0, .)} %>%
+            rev
+    }
+}
+
+
+df_epidemiology$deaths <- NA
+daily_deaths_vector <- daily_deaths_vector_factory(df_epidemiology)
+
+for (country_name in unique(df_epidemiology$country)) {
+    mask <- df_epidemiology$country == country_name
+    df_epidemiology[mask, "deaths"] <- daily_deaths_vector(country_name)
+}
+
 
 ## To get a print out of the structure of the data frame we have just
 ## constructed use the following
@@ -27,9 +56,41 @@ df_epidemiology <- read.csv(url_str,
 ## > head(df_epidemiology)
 ##
 
-plot_df <- data.frame(date = tail(rev(df_epidemiology$date), -1),
-                      daily_deaths = diff(rev(df_epidemiology$dead)))
 
-g <- ggplot(plot_df, aes(x = date, y = daily_deaths)) + geom_point()
+## The data is loaded into the shiny app used by COMO from a file
+## \code{cases.Rda} and this appears to happen once each time the application is
+## started. A simple solution would just be to replace the line at the following
+## URL with the code to load in the CSV from OxCDB.
+##
+## https://github.com/ocelhay/como/blob/master/inst/comoapp/www/source_on_inception.R#L21
+##
+## So we have a point of comparison, we will load in the data that the
+## application is currently using and then format the OxCDB data in the same
+## way.
 
+
+cases_rda_url <- "https://github.com/ocelhay/como/raw/master/inst/comoapp/www/data/cases.Rda"
+
+tmp <- tempfile()
+GET(url = cases_rda_url, write_disk(tmp))
+load(tmp)
+
+
+## Make a little plot to check that both data sets look similar.
+
+g <- ggplot(mapping = aes(x = date, y = deaths)) +
+    geom_line(data = filter(cases, country == "United_Kingdom"), colour = "blue") +
+    geom_point(data = filter(df_epidemiology, country == "United_Kingdom"), colour = "red")
 print(g)
+
+## Looks good. So to use the OxCDB data instead of the current RDA file, the
+## code above constructing \code{df_epidemiology} could replace line 21 in the
+## file linked above and things should work, assuming the order of the records
+## in the data frame does not matter. There is only a single country that has a
+## name that is mangled, see the code below for example.
+
+
+## To check which countries have different names there is the following
+cases_country_names <- unique(cases$country)
+df_epi_country_names <- unique(df_epidemiology$country)
+print(setdiff(cases_country_names, df_epi_country_names))
